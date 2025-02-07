282a9a2677edaa415c7b756b288355725501ce88
vijit-vishnoi <vishnoivijit@gamil.com>
2025-02-08 02:20:13 +0530

    Added v1 for tweets model and hashtag model

--- src/index.js
+++ src/index.js
@@ -9,15 +9,6 @@ app.listen(3000,async()=>{
     console.log('server started');
     await connect(); 
     console.log('Mongo db connected');
-    // const tweet=await Tweet.create({
-    //     content: 'third tweet',
-    // });
-    // const tweets=await Tweet.find({userEmail:'a@b.com'});
-    const tweetRepo=new TweetRepository();
-    const tweet=await tweetRepo.create({content:'With hooks now'});
-    console.log(tweet);
-    // const tweet=await tweetRepo.getAll(0,4);
-    // console.log(tweet[0].contentWithEmail);
 
 });
 
