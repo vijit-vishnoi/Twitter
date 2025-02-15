50b8f34397a1d86a9ec7839b08aba5f59d2aa1ac
vijit-vishnoi <vishnoivijit@gamil.com>
2025-02-16 03:18:35 +0530

    Protected comment creation also

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -12,7 +12,7 @@ router.post('/tweets',authenticate,createTweet);
 router.get('/tweets/:id',getTweet);
 
 router.post('/likes/toggle',toggleLike);
-router.post('/comments',createComment);
+router.post('/comments',authenticate,createComment);
 router.post('/signup',signup);
 router.post('/login',login);
 
